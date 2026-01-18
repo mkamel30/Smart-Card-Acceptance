8a08e33dee050f1570ea506f29d046725a5b7408
Mostafa Kamel <mkamel30@gmail.com>
2026-01-18 12:35:15 +0200

    fix: resolve typescript unused parameter error to fix production build

--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -38,7 +38,7 @@ export const authenticate = (req: Request, res: Response, next: NextFunction) =>
     }
 };
 
-export const optionalAuthenticate = (req: Request, res: Response, next: NextFunction) => {
+export const optionalAuthenticate = (req: Request, _res: Response, next: NextFunction) => {
     console.log(`[Auth] Optional Authenticate for ${req.method} ${req.path}`);
     // 1. Check for Legacy Admin Password
     const adminPassword = req.headers['x-admin-password'];
